bb1c0c75ebfddd2253797f8cfdf2f3ad1eea7bb2
s21dororebe <s21dororebe@venta.lv>
2023-05-18 14:01:59 +0300

    DONE: constructors, validations

---
+++ src/main/java/lv/venta/models/Professor.java
@@ -0,0 +1,41 @@
+package lv.venta.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Table (name = "professor_table")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Professor {
+    @Column(name="Idp")
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO) //auto generation
+    @Setter(value=AccessLevel.NONE) //no id setter
+    private long idp;
+
+    @Column(name="name")
+    @NotNull
+    @Size(min=3, max=20)
+    @Pattern(regexp ="[A-Z]{1}[a-z]+", message = "1.burtam jabut lielam un tikai latinu burti")
+    private String name;
+
+    @Column(name="surname")
+    @NotNull
+    @Size(min=3, max=20)
+    @Pattern(regexp ="[A-Z]{1}[a-z]+", message = "1.burtam jabut lielam un tikai latinu burti")
+    private String surname;
+
+    @Column(name="degree")
+    @NotNull
+    private Degree degree;
+
+    public Professor(String name, String surname, Degree degree) {
+        this.name = name;
+        this.surname = surname;
+        this.degree = degree;
+    }
+}
