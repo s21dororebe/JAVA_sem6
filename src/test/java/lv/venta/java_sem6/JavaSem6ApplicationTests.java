0bbf8a0ebfb74f2b19fbdebf8e4352aa092e564c
s21dororebe <s21dororebe@venta.lv>
2023-05-18 13:19:23 +0300

    New project

---
+++ src/test/java/lv/venta/java_sem6/JavaSem6ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.venta.java_sem6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSem6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
