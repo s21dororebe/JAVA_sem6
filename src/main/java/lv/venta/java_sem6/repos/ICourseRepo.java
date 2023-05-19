f3694d4c86f03d0d4a4ec443dfc9f05b560ee967
s21dororebe <s21dororebe@venta.lv>
2023-05-19 15:36:41 +0300

    Edit: added functions

--- src/main/java/lv/venta/java_sem6/repos/ICourseRepo.java
+++ src/main/java/lv/venta/java_sem6/repos/ICourseRepo.java
@@ -3,6 +3,11 @@ package lv.venta.java_sem6.repos;
 import lv.venta.java_sem6.models.Course;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ICourseRepo extends CrudRepository<Course, Long> {
 
+    ArrayList<Course> findByGradesStudentIds(long id);
+
+    ArrayList<Course> findAllByProfessorsIdp(long id);
 }
--- src/main/java/lv/venta/java_sem6/repos/IGradeRepo.java
+++ src/main/java/lv/venta/java_sem6/repos/IGradeRepo.java
@@ -3,6 +3,12 @@ package lv.venta.java_sem6.repos;
 import lv.venta.java_sem6.models.Grade;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface IGradeRepo extends CrudRepository<Grade, Long> {
 
+
+    ArrayList<Grade> findByStudentIds(long id);
+
+    ArrayList<Grade> findByCourseIdc(long id);
 }
