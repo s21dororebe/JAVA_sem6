a7b875adf559b32396d69d0fca5df2778640fc17
s21dororebe <s21dororebe@venta.lv>
2023-05-19 14:32:30 +0300

    New: filtering service

---
+++ src/main/java/lv/venta/java_sem6/services/IFilteringService.java
@@ -0,0 +1,17 @@
+package lv.venta.java_sem6.services;
+
+import lv.venta.java_sem6.models.Course;
+import lv.venta.java_sem6.models.Grade;
+import lv.venta.java_sem6.models.Professor;
+import lv.venta.java_sem6.models.Student;
+
+import java.util.ArrayList;
+
+public interface IFilteringService {
+    ArrayList<Student> retrieveAllStudents();
+    ArrayList<Grade> retrieveAllGrades();
+    ArrayList<Professor> retrieveAllProfessors();
+    ArrayList<Course> retrieveAllCourses();
+    ArrayList<Grade> retrieveAllGradesByStudentId(long id);
+
+}
